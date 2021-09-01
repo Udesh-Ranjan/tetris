80b80f12825030a33cc0c93db4481901576f29a1
udesh-ranjan <ranjanudesh404@gmail.com>
2021-09-01 16:38:32 +0530

    EnvironConstants added

--- src/tetris/application/GameApplication.java
+++ src/tetris/application/GameApplication.java
@@ -10,11 +10,60 @@ import javafx.scene.input.KeyCombination;
 
 import tetris.util.EnvironmentConstants;
 
+import javafx.scene.layout.HBox;
+import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
+import javafx.scene.Group;
+import javafx.scene.effect.ColorInput;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.CornerRadii;
+import javafx.geometry.Insets;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Background;
+
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.List;
+
 public class GameApplication extends Application{
+	private Thread thread;
+	private volatile boolean running;
+	private void startGame(){
+		running=true;
+		thread=new Thread(new Game());
+		thread.start();
+	}
+	private void stopGame(){
+		running=false;
+	}
 	@Override
 	public void start(Stage stage){
-		StackPane stackPane=new StackPane();
-		Scene scene=new Scene(stackPane,500,500);
+		//StackPane stackPane=new StackPane();
+		HBox hbox=new HBox();
+		Pane left,center,right;
+		left=new Pane();
+		center=new Pane();
+		ColorInput ci = new ColorInput(center.getLayoutX(),
+				center.getLayoutY(),
+				center.getLayoutBounds().getWidth(),
+				center.getLayoutBounds().getHeight(),
+				Color.RED);
+		center.prefWidth(400);
+		center.prefHeight(400);
+		//center.setEffect(ci);
+		center.setStyle("-fx-background-color: red;");
+		Rectangle r = new Rectangle(25,25,250,250);
+		r.setFill(Color.BLUE);
+		center.getChildren().add(r);
+		right=new Pane();
+		hbox.getChildren().addAll(left,center,right);
+		//hbox.getStyleClass().add("jfx-decorator-buttons-container");
+		hbox.setBackground(new Background(new BackgroundFill(Color.BLACK,
+						CornerRadii.EMPTY,
+						Insets.EMPTY)));
+		Scene scene=new Scene(hbox,500,500);
 		stage.setScene(scene);
 		stage.setTitle("Tetris Game");
 		stage.setFullScreen(true);
@@ -29,11 +78,33 @@ public class GameApplication extends Application{
 					stage.setFullScreen(true);
 			}
 		});
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>(){
+			@Override
+			public void handle(WindowEvent winEvent){
+				stopGame();
+				System.out.println("Exiting");
+				System.exit(0);
+			}
+		});
 		stage.show();
 		System.out.println("SCREEN_WIDTH : "+EnvironmentConstants.SCREEN_WIDTH);
 		System.out.println("SCREEN_HEIGHT : "+EnvironmentConstants.SCREEN_HEIGHT);
+		System.out.println("Size : "+EnvironmentConstants.computeEnvironSize());
+		startGame();
 	}
 	public static void main(String ...$){
 		launch($);
+	}
+	public class Game implements Runnable{
+		@Override
+		public void run(){
+			try{
+				while(running){
+					Thread.sleep(100);
+				}
+			}catch(final InterruptedException exception){
+				exception.printStackTrace();
+			}
+		}
 	}
 }
--- src/tetris/util/EnvironmentConstants.java
+++ src/tetris/util/EnvironmentConstants.java
@@ -2,7 +2,42 @@ package tetris.util;
 
 import javafx.stage.Screen;
 
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.List;
+
 public class EnvironmentConstants{
 	public static final double SCREEN_WIDTH=Screen.getPrimary().getBounds().getWidth();
-	public static final double  SCREEN_HEIGHT=Screen.getPrimary().getBounds().getHeight();
+	public static final double SCREEN_HEIGHT=Screen.getPrimary().getBounds().getHeight();
+	public static final byte HORIZONTAL_BLOCKS=16;
+	public static final byte VERTICAL_BLOCKS=14;
+	public static final int LEFT_MIN_SIZE_PIXELS=250; 
+	public static final byte  RIGHT_MIN_SIZE_BLOCKS=2;
+
+	public static List<Integer> computeEnvironSize(){
+		int pixelsPerBlock=50;
+		int pixels=EnvironmentConstants.LEFT_MIN_SIZE_PIXELS+EnvironmentConstants.HORIZONTAL_BLOCKS*pixelsPerBlock+
+			EnvironmentConstants.RIGHT_MIN_SIZE_BLOCKS*pixelsPerBlock;
+		if(pixels > EnvironmentConstants.SCREEN_WIDTH || 
+				pixelsPerBlock*EnvironmentConstants.VERTICAL_BLOCKS > EnvironmentConstants.SCREEN_HEIGHT){
+			do{
+				pixelsPerBlock--;
+				if(pixelsPerBlock==0){
+					System.out.println("screen size is less to render the game");
+					System.exit(0);
+				}
+				pixels=EnvironmentConstants.LEFT_MIN_SIZE_PIXELS+EnvironmentConstants.HORIZONTAL_BLOCKS*pixelsPerBlock+
+					EnvironmentConstants.RIGHT_MIN_SIZE_BLOCKS*pixelsPerBlock;
+			}while(pixels>EnvironmentConstants.SCREEN_WIDTH || 
+					pixelsPerBlock*EnvironmentConstants.VERTICAL_BLOCKS>EnvironmentConstants.SCREEN_HEIGHT);
+		}else{
+		}
+		int pixelsLeft=(int)EnvironmentConstants.SCREEN_WIDTH-
+			(pixelsPerBlock*EnvironmentConstants.HORIZONTAL_BLOCKS+EnvironmentConstants.LEFT_MIN_SIZE_PIXELS+pixelsPerBlock*EnvironmentConstants.RIGHT_MIN_SIZE_BLOCKS);
+		int leftSizePixels=EnvironmentConstants.LEFT_MIN_SIZE_PIXELS+pixelsLeft/2;
+		int centerSizePixels=pixelsPerBlock*EnvironmentConstants.HORIZONTAL_BLOCKS;	
+		int rightSizePixels=(int)EnvironmentConstants.SCREEN_WIDTH-(leftSizePixels+centerSizePixels);
+		return Stream.of(leftSizePixels,centerSizePixels,rightSizePixels)
+			.collect(Collectors.toList());
+	}
 }
